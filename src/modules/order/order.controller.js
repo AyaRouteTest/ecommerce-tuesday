62a36ace2dd81eca69e80a422c9470194bc0e0cb
Ayanabilothman <aya.nabil.othman@gmail.com>
2023-08-31 17:53:19 +0300

    webhook

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -183,3 +183,28 @@ export const cancelOrder = asyncHandler(async (req, res, next) => {
 
   return res.json({ success: true, message: "order canceled successfully!" });
 });
+
+export const webhook = async (req, res) => {
+  const stripe = new Stripe(process.env.STIPE_KEY);
+  const sig = req.headers["stripe-signature"];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.ENDPOINT_SECRET
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  const orderId = event.data.object.metadata.orderId;
+  if (event.type === "checkout.session.completed") {
+    await Order.findByIdAndUpdate(orderId, { status: "payed" });
+    return;
+  }
+  await Order.findByIdAndUpdate(orderId, { status: "failed payment" });
+  return;
+};
--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -3,6 +3,7 @@ import { isValid } from "./../../middleware/validation.middleware.js";
 import { isAuthenticated } from "./../../middleware/authentication.middleware.js";
 import { cancelOrderSchema, createOrderSchema } from "./order.validation.js";
 import { createOrder, cancelOrder } from "./order.controller.js";
+import express from "express";
 const router = Router();
 
 // create order
@@ -14,6 +15,12 @@ router.patch(
   isAuthenticated,
   isValid(cancelOrderSchema),
   cancelOrder
+);
+
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  asyncHandler(order.webhook)
 );
 
 export default router;
